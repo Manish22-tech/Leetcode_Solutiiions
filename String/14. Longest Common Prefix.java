3028eba6b741b1ebdfe4ef0d0ab0876ab819f432
Manish Yadav <yadavmanish9617@gmail.com>
2025-12-11 17:52:39 +0530

    Implement longest common prefix function

---
+++ String/14. Longest Common Prefix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+        String first = strs[0];
+
+        for (int i = 0; i < first.length(); i++) {
+            char ch = first.charAt(i);
+
+            for (int j = 1; j < strs.length; j++) {
+                // If index exceeds length OR characters don't match
+                if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
+                    return first.substring(0, i);
+                }
+            }
+        }
+        return first;
+    }
+}
